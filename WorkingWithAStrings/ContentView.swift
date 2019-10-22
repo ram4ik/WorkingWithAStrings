1c34275d4dbb9bd1d535a0dfeffdbb084d723b49
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-22 11:46:46 +0300

    trimmed

--- WorkingWithAStrings/ContentView.swift
+++ WorkingWithAStrings/ContentView.swift
@@ -10,7 +10,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        
+        // Step 1
+        let input = "a b c"
+        let letters = input.components(separatedBy: " ")
+        
+        // Step 2
+        let input2 = """
+                     a
+                     b
+                     c
+                     """
+        let letters2 = input2.components(separatedBy: "\n")
+        
+        // Random option element
+        let letter = letters.randomElement()
+        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        
+        let letter2 = letters2.randomElement()
+        let trimmed2 = letter2?.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        return Text("Hello World")
     }
 }
 
